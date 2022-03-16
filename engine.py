cb0dcc17211190929e5aab8065a69be44bb49729
raaaandom <itzraandom@gmail.com>
2022-03-16 17:26:31 +0100

    Basic event listener

--- engine.py
+++ engine.py
@@ -11,10 +11,21 @@ GAMEWINDOW_SIZE = (GAMEWINDOW_WIDTH,GAMEWINDOW_HEIGHT)
 #Game window
 gameWindow = pygame.display.set_mode(GAMEWINDOW_SIZE)
 
+#Event catcher
+def catchEvents():
+    for e in pygame.event.get():
+        
+        if e.type == pygame.QUIT:
+            global gameWindowStatus
+            gameWindowStatus = False
+
+        #Catch other events here
+
 #Main loop
 gameWindowStatus = True
 while gameWindowStatus:
-    pass
+    
+    catchEvents()
 
 #Unload pygame
 pygame.quit()
